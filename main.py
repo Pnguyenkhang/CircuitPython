60dd39061aaf19b19673bafc70145454891e3c8e
Peter Nguyen <37032927+Pnguyenkhang@users.noreply.github.com>
2021-10-07 02:45:50 -0700

    for light changes

--- main.py
+++ main.py
@@ -8,39 +8,35 @@ import adafruit_dotstar as dotstar
 import time
 import neopixel
 import digitalio
+import adafruit_hcsr04
 
+sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D4, echo_pin=board.D3)
 
+'''
 # Built in LED
 led = digitalio.DigitalInOut(board.D13)
 led.direction = digitalio.Direction.OUTPUT
-# Capacitive touch on D3
-touch = touchio.TouchIn(board.D3)
 # Digital input with pullup on D4
-button1 = DigitalInOut(board.D4)
-button1.direction = Direction.INPUT
-button1.pull = Pull.UP
-button1_in = True
+button4 = DigitalInOut(board.D4)
+button4.direction = Direction.OUTPUT
+button3 = DigitalInOut(board.D3)
+button3.direction = Direction.OUTPUT
+'''
+
+
+
+# Helper to convert analog input to voltage
+def getVoltage(pin):
+    return (pin.value * 3.3) / 65536
 
 # one pixel connected internally
 dot = dotstar.DotStar(board.APA102_SCK, board.APA102_MOSI, 1, brightness=0.2)
 
-RequiredTimeInState = 10
-TimeInState = 0
-count = 0
-prevState = False
-currState = False
 while True:
-    while currState == prevState:
-        currState = button1.value
-        TimeInState += 1
-    if currState and TimeInState > RequiredTimeInState:
-        # Code to do when button first pressed
-        count+=1
-        print("Number of times pressed",count)
-    else:
-        if TimeInState > RequiredTimeInState:
-            # Code to do when button released
-            print("button released")
-    prevState = currState
-    TimeInState = 0
+    try:
+        print((sonar.distance,))
+    except RuntimeError:
+        print("Retrying!")
+    time.sleep(0.1)
+
 
